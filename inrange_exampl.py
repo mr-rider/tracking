7e54be105f91a4c980bffa1095ccad6524ff47d6
mr-rider <mr.rider2@mail.ru>
2021-06-01 00:02:30 +0800

    add test prog for tracking color objects

---
+++ inrange_exampl.py
@@ -0,0 +1,37 @@
+"""
+Tracking specific colors.
+"""
+import numpy as np
+import cv2
+
+
+def emptyFunction():
+    pass
+
+
+cap = cv2.VideoCapture(0) # capture video from web cam
+
+windowName = 'Object Tracker'
+trackbarName = 'Color Chooser'
+
+cv2.namedWindow(windowName)
+cv2.createTrackbar(trackbarName, windowName, 0, 1, emptyFunction)
+
+color = 0
+
+while(True):
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    color = cv2.getTrackbarPos(trackbarName, windowName)
+    if color == 0:
+        image_mask = cv2.inRange(hsv, np.array([40, 50, 50]), np.array([95, 255, 255])) # green
+    else:
+        image_mask = cv2.inRange(hsv, np.array([100, 50, 50]), np.array([140, 255, 255])) # blue
+    output = cv2.bitwise_and(frame, frame, mask=image_mask)
+    cv2.imshow(windowName, output)
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
