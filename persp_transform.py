986fcf509e8eb4ed9efdd0168fb2718844ce9904
mr-rider <mr.rider2@mail.ru>
2021-06-01 23:48:01 +0800

    example of perspective transformation of image

---
+++ persp_transform.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+image = cv2.imread('/home/mr-rider/Robot/book/datasets/misc/ruler.512.tiff', 1)
+input_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+rows, cols, channels = input_image.shape
+
+points1 = np.float32([[0, 0], [400, 0], [0, 400], [400, 400]])
+points2 = np.float32([[0,0], [300, 0], [0, 300], [300, 300]])
+
+P = cv2.getPerspectiveTransform(points1, points2)
+output = cv2.warpPerspective(input_image, P, (300, 300))
+
+plt.subplot(121)
+plt.imshow(input_image)
+plt.title('Input Image')
+
+plt.subplot(122)
+plt.imshow(output)
+plt.title('Perspective Transform')
+plt.show()
