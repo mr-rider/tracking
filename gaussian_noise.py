a1b4f59525812dc2b32c8c412a1830b00f504d83
mr-rider <mr.rider2@mail.ru>
2021-06-07 23:43:30 +0800

    noise examples

---
+++ gaussian_noise.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('/home/mr-rider/Robot/book/datasets/misc/4.1.03.tiff', 0)
+row, col = img.shape
+img = img.astype(np.float32)
+mean = 0
+var = 0.1
+sigma = var**0.5
+gauss = np.random.normal(mean, sigma, (row, col))
+gauss = gauss.reshape(row, col)
+noisy = img + gauss
+
+print(abs(noisy-img))
+
+plt.imshow(noisy, cmap='gray')
+plt.title('Gaussian (Normally distributed) Noise')
+plt.axis('off')
+plt.show()
